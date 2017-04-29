ecba71b0ac43bec1210a4ca1ec8eb955858c7c9f
nehap12 <neha.paul@sjsu.edu>
2017-04-28 21:37:19 -0700

    social authentication using firebase

--- Facebook and Google Authentication/src/app/app.module.ts
+++ Facebook and Google Authentication/src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
+
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Facebook and Google Authentication/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+
+export const router: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'members', component: MembersComponent, canActivate: [AuthGuard] }
+
+]
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
